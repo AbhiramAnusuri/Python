f9d8b09c2938e1485899d66107c39a4ec983d73c
AbhiramAnusuri <69586687+AbhiramAnusuri@users.noreply.github.com>
2023-01-15 23:29:44 +0530

    Challenge 2: Implement a Calculator Class
    In this exercise, you have to implement a calculator that can perform addition, subtraction, multiplication, and division.
    
    Problem statement Write a Python class called Calculator by completing the tasks below:
    
    Task 1
    
    👉 Initializer
    Implement an initializer to initialize the values of num1 and num2. Properties
    • num1
    • num2
    
    Task 2
    
    👉 Methods
    • add() is a method that returns the sum of num1 and num2.
    • subtract() is a method that returns the subtraction of num1 from num2.
    • multiply() is a method that returns the product of num1 and num2.
    • divide() is a method that returns the division of num2 by num1.
    Input - Pass numbers (integers or floats) in the initializer.
    
    Output - addition, subtraction, division, and multiplication

---
+++ opps_assignment_challenge_2.py
@@ -0,0 +1,18 @@
+class Calculator:
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+    def add(self):
+        return self.num2+self.num1
+    def subtract(self):
+        return self.num2-self.num1
+    def multiply(self):
+        return self.num2*self.num1
+    def divide(self):
+        return self.num2/self.num1
+obj=Calculator(10,94)
+print(obj.add())
+print(obj.subtract())
+print(obj.multiply())
+print(obj.divide())
+        
