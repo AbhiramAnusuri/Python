7346f6565ccaa7e3d3a086e2c85f7deeba133ff0
AbhiramAnusuri <69586687+AbhiramAnusuri@users.noreply.github.com>
2023-02-04 23:27:32 +0530

    Opps assignment on Dog
    Create a class named ‘Dog’. It should have a constructor which accepts its name, age and coat color. You must perform the following operations:
    🔴 a. It should have a function ‘description()’ which prints the name and age of the dog.
    🔴 b. It should have a function ‘get_info()’ which prints the coat color of the dog.
    🔴 c. Create child classes ‘JackRussellTerrier’ and ‘Bulldog’ which is inherited from the class ‘Dog’. It should have at least two methods of its own.
    🔴 d. Create objects and implement the above functionalities.

---
+++ Dog.py
@@ -0,0 +1,36 @@
+class Dog:
+    def __init__(self,name,age,coat_color):
+        self.name=name
+        self.age=age
+        self.coat_color=coat_color
+    def description(self):
+        print(f"name is : {self.name} \n age is : {self.age} ")
+    def get_info(self):
+        print(f"coat color is: {self.coat_color}")
+class JackRusellTerrier(Dog):
+                def __init__(self,name,age,coat_color):
+                    super().__init__(name,age,coat_color)
+                def is_hyper(self):
+                    print(f"{self.name} is a hyper Jack Russell Terrier!")
+                def is_small(self):
+                    print(f"{self.name} is a small dog.")
+        
+class Bulldog(Dog):
+    def __init__(self, name, age, coat_color):
+        super().__init__(name, age, coat_color)
+        
+    def is_strong(self):
+        print(f"{self.name} is a strong Bulldog.")     
+    def is_loyal(self):
+        print(f"{self.name} is a loyal Bulldog.")
+dog1=JackRusellTerrier("max",5,"brown and white")
+dog1.description()
+dog1.get_info()
+dog1.is_hyper()
+dog1.is_small()
+
+dog2=Bulldog("buddy",8,"brindle")
+dog2.description()
+dog2.get_info()
+dog2.is_strong()
+dog2.is_loyal()
